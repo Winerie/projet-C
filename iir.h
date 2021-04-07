274698fb29e7c63417bf86ab0d37300fd9eabd99
tristanlecoz <55884868+tristanlecoz@users.noreply.github.com>
2021-04-07 12:08:44 +0200

    Add files via upload
    concaténation fir et irr

--- iir.h
+++ iir.h
@@ -5,6 +5,9 @@
 * VERSION: 1.0 
 ************************************************************************/
 #include "define.h"
+#include "fichiers.h"
 
 
 absorp iirTest(char* filename);
+absorp init_Absorp(absorp myabsorp);
+absorp iir(absorp xn, absorp xn_1, absorp *yn_1);
