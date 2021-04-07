00af3287272e31eae8fd42cb268ce5f29536549d
Winerie <justine6leven@gmail.com>
2021-04-07 11:16:38 +0200

    version irr

--- main.c
+++ main.c
@@ -6,10 +6,15 @@
 ************************************************************************/
 
 #include "affichage.h"
+#include "iir.h"
 
 int main (){
     oxy test;
+   
+    absorp myAbsorp;
     test.pouls=100;
     test.spo2=105;
     affichage(test);
+    myAbsorp = iirTest("record1_fir.dat");
+    printf("%f %f", myAbsorp.acr, myAbsorp.acir);
 }
